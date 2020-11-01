0b986393353610624c52b7a0909a5c0a4ccca594
maxisui <55889261+maxisui@users.noreply.github.com>
2020-11-01 15:52:10 +0100

    recetn updates

--- Exercise_10_List_Overlap_Comprehensions.py
+++ Exercise_10_List_Overlap_Comprehensions.py
@@ -22,6 +22,15 @@ Extra:
 '''
 __author__: 'maxisui'
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-blabla
+import random
+
+a = random.sample(range(1,30), 12)
+b = random.sample(range(1,30), 12)
+
+overlap = [i for i in set(a) if i in b]
+result = []
+
+for element in overlap:
+    if element not in result:
+        result.append(element)
+print(result)
---
+++ Exercise_11_Check_Primality_Functions.py
@@ -0,0 +1,14 @@
+'''
+from www.practicepython.org
+completed on 01.11.2020
+
+Ask the user for a number and determine whether the number is prime or not. 
+(For those who have forgotten, a prime number is a number that has no divisors.). 
+You can (and should!) use your answer to Exercise 4 to help you. 
+Take this opportunity to practice using functions, described below.
+'''
+__author__: 'maxisui'
+
+import pandas 
+
+

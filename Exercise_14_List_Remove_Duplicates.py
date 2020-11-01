16e80127168e320906d2054a7c9001d253d62187
maxisui <55889261+maxisui@users.noreply.github.com>
2020-11-01 22:12:47 +0100

    recent projects

---
+++ Exercise_14_List_Remove_Duplicates.py
@@ -0,0 +1,12 @@
+''''
+from www.practicepyton.com
+completed on 01.11.2020
+
+Write a program (function!) that takes a list and returns a new list that contains all the elements of the first list minus all the duplicates.
+
+Extras:
+Write two different functions to do this - one using a loop and constructing a list, and another using sets.
+Go back and do Exercise 5 using sets, and write the solution for that in a different function.
+'''
+__author__: 'maxisui'
+
